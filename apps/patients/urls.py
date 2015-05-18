331656e2957aa46415b90a95ef7584c06bcf6edb
Alvaro <thesunno@gmail.com>
2015-05-17 23:40:41 -0430

    classed views

--- apps/patients/urls.py
+++ apps/patients/urls.py
@@ -1,10 +1,13 @@
+from apps.patients.views import PatientList, PatientDetail, PatientEdit, PatientNew
+from django.contrib.auth.decorators import login_required
+
 __author__ = 'alvaro'
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'apps.patients.views',
-    url(r'^$', 'patient_list', name='patient_list'),
-    url(r'^create/$', 'patient_create', name='patient_create'),
-    url(r'^(\d+)/$', 'patient_detail', name='patient_detail'),
-    url(r'^(\d+)/edit/$', 'patient_edit', name='patient_edit')
+    url(r'^$', login_required(PatientList.as_view()), name='patient_list'),
+    url(r'^new/$', login_required(PatientNew.as_view()), name='patient_create'),
+    url(r'^(\d+)/$', login_required(PatientDetail.as_view()), name='patient_detail'),
+    url(r'^(\d+)/edit/$', login_required(PatientEdit.as_view()), name='patient_edit')
 )
--- apps/patients/views.py
+++ apps/patients/views.py
@@ -1,8 +1,8 @@
 from apps.patients.forms import PatientBaseForm
 from apps.patients.models import Patient
-from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.views.generic.base import View
 from faker import Factory
 
 fake = Factory.create()
@@ -11,36 +11,92 @@ fake = Factory.create()
 from django.template.context import RequestContext
 
 
-@login_required
-def patient_list(request):
-    return HttpResponse(render(request, 'patient_list.html',
-                               context=RequestContext(request)))
+class PatientList(View):
+    form_class = PatientBaseForm
+
+    def get(self, request):
+        return HttpResponse(render(request, 'patient_list.html',
+                                   context=RequestContext(request)))
+
+    def post(self, request):
+        """
+        Process user creation
+        :param request:
+        :return:
+        """
+        # processing form
+        form = self.form_class(request.POST, request.FILES)
+
+        patient = None
+        if form.is_valid():
+            patient = form.save()
+
+        data = {
+            'patient': patient,
+            'form': form
+        }
+        return HttpResponse(render(request, 'patient_detail.html', context=RequestContext(request, data)))
 
 
-@login_required
-def patient_detail(request, patient_id):
-    patient = Patient.objects.get(pk=1)
-    return HttpResponse(render(request, 'patient_detail.html',
-                               context=RequestContext(request, {'patient': patient})))
+class PatientDetail(View):
+    form_class = PatientBaseForm
+
+    def get(self, request, patient_id):
+        patient = Patient.objects.get(pk=1)
+        return HttpResponse(render(request, 'patient_detail.html',
+                                   context=RequestContext(request, {'patient': patient})))
+
+    def post(self, request, patient_id):
+        """
+        Saves an user edit
+        :param request:
+        :param patient_id:
+        :return:
+        """
+        patient = Patient.objects.get(pk=1)
+        # processing form
+        form = self.form_class(request.POST, request.FILES, instance=patient)
+
+        if form.is_valid():
+            patient = form.save()
+
+        data = {
+            'patient': patient,
+            'form': form
+        }
+        return HttpResponse(render(request, 'patient_detail.html', context=RequestContext(request, data)))
 
 
-@login_required
-def patient_edit(request, patient_id):
-    patient = Patient.objects.get(pk=1)
-    form = PatientBaseForm(instance=patient)
-    data = {
-        'patient': patient,
-        'form': form
-    }
-    return HttpResponse(render(request, 'patient_detail.html',
-                               context=RequestContext(request, data)))
+class PatientEdit(View):
+    form_class = PatientBaseForm
+
+    def get(self, request, patient_id):
+        """
+        Shows patient edit form
+        :param request:
+        :param patient_id:
+        :return:
+        """
+        patient = Patient.objects.get(pk=1)
+        form = self.form_class(instance=patient)
+        data = {
+            'patient': patient,
+            'form': form
+        }
+        return HttpResponse(render(request, 'patient_detail.html', context=RequestContext(request, data)))
 
 
-@login_required
-def patient_create(request):
-    form = PatientBaseForm()
-    data = {
-        'form': form
-    }
-    return HttpResponse(render(request, 'patient_detail.html',
-                               context=RequestContext(request, data)))
+class PatientNew(View):
+    form_class = PatientBaseForm
+
+    def get(self, request):
+        """
+        Shows patient creation form
+        :param request:
+        :return:
+        """
+        form = self.form_class()
+        data = {
+            'form': form
+        }
+        return HttpResponse(render(request, 'patient_detail.html', context=RequestContext(request, data)))
