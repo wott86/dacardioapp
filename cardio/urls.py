3288b45bf8f658174d7b370f473b6cdc59c092c9
Alvaro <thesunno@gmail.com>
2015-05-17 22:42:24 -0430

    main url redirects to patient list, for now

--- cardio/urls.py
+++ cardio/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'cardio.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
 
+                       url(r'^$', RedirectView.as_view(url='patients/')),
                        url(r'^admin/', include(admin.site.urls)),
 
                        url(r'^users/', include('apps.users.urls')),
