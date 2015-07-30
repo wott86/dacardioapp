8c47a43f346b51a0b41c186ae72da20002081da2
Alvaro <thesunno@gmail.com>
2015-07-29 22:47:17 -0430

    updated requirements pypy compatibility

--- cardio/settings.py
+++ cardio/settings.py
@@ -7,7 +7,12 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-
+try:
+    import psycopg2
+except ImportError:
+    # Fall back to psycopg2cffi
+    from psycopg2cffi import compat
+    compat.register()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
