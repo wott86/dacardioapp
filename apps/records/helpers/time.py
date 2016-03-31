545e2861a0ea447a8ab577ad94f79d8b71a0d419
Alvaro <thesunno@gmail.com>
2016-03-31 00:40:55 -0430

    Some docstrings

--- apps/records/helpers/time.py
+++ apps/records/helpers/time.py
@@ -5,5 +5,11 @@ TIME_MULTIPLIER = {
 
 
 def convert_time_to_milli(channel, date_obj):
+    """
+    convert_time_to_milli
+
+    :param channel: apps.records.models.Channel
+    :param date_obj: datetime.datetime
+    """
     diff = float((date_obj - channel.start_date).total_seconds()) * 1000
     return diff if diff >= 0 else 0
